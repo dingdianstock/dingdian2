fc8e628cafdfc57f956ffd5fad418b6f0b119f33
mac <mac>
2024-02-07 20:38:32 +0800

    Finally got angular to work with express partial routes

--- models/user.js
+++ models/user.js
@@ -1,85 +1,34 @@
 var mongoose = require('mongoose'),
-    Schema = mongoose.Schema,
-    crypto = require('crypto'),
-    _ = require('underscore');
+  bcrypt = require('bcrypt');
 
-
-var UserSchema = new Schema({
-  name: String,
-  email: String,
-  username: {
-    type: String,
-    unique: true
-  },
-  provider: String,
-  hashed_password: String,
-  salt: String,
-  facebook: {},
-  twitter: {},
-  google: {}
+var userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true},
+  admin: { type: Boolean, required: true }
 });
 
-/**
- * Virtuals
- */
-UserSchema.virtual('password').set(function(password) {
-  this._password = password;
-  this.salt = this.makeSalt();
-  this.hashed_password = this.encryptPassword(password);
-}).get(function() {
-    return this._password;
+userSchema.pre('save', function(next) {
+  var user = this;
+
+  if (!user.isModified('password')) return next();
+
+  bcrypt.genSalt(32, function(err, salt) {
+    if (err) return next(err);
+
+    bcrypt.hash(user.password, salt, function(err, hash) {
+      if (err) return next(err);
+      user.password = hash;
+      next();
+    });
   });
-
-
-
-/**
- * Pre-save hook
- */
-UserSchema.pre('save', function(next) {
-  if (!this.isNew) return next();
-
-  if (!validatePresenceOf(this.password) && authTypes.indexOf(this.provider) === -1)
-    next(new Error('Invalid password'));
-  else
-    next();
 });
 
-/**
- * Methods
- */
-UserSchema.methods = {
-  /**
-   * Authenticate - check if the passwords are the same
-   *
-   * @param {String} plainText
-   * @return {Boolean}
-   * @api public
-   */
-  authenticate: function(plainText) {
-    return this.encryptPassword(plainText) === this.hashed_password;
-  },
-
-  /**
-   * Make salt
-   *
-   * @return {String}
-   * @api public
-   */
-  makeSalt: function() {
-    return Math.round((new Date().valueOf() * Math.random())) + '';
-  },
-
-  /**
-   * Encrypt password
-   *
-   * @param {String} password
-   * @return {String}
-   * @api public
-   */
-  encryptPassword: function(password) {
-    if (!password) return '';
-    return crypto.createHmac('sha1', this.salt).update(password).digest('hex');
-  }
+userSchema.methods.comparePassword = function(candidatePassword, cb) {
+  bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+    if(err) return cb(err);
+    cb(null, isMatch);
+  });
 };
 
-mongoose.model('User', UserSchema);
+module.exports = mongoose.model('User', userSchema);
